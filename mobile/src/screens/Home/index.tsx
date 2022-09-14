ee9d8614bddd89fd57ed1fd528245c5977f8b8b2
Odamiani <feldamiani@gmail.com>
2022-09-14 19:30:38 -0300

    atualização

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,9 +1,9 @@
 import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from "../../components/GameCard";
+import { GameCard } from '../../components/GameCard';
 
-import { Heading } from "../../components/Heading";
+import { Heading } from '../../components/Heading';
 
 import { GAMES } from '../../utils/games';
 import { styles } from './styles';
@@ -11,28 +11,34 @@ import { styles } from './styles';
 export function Home() {
   return (
     <View style={styles.container}>
-      <Image
-        source={logoImg}
-        style={styles.logo}
+      <Image 
+      source={logoImg}
+      style={styles.logo}
       />
 
-      <Heading
+      <Heading 
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar..."
       />
 
+      
       <FlatList
-        data={GAMES}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <GameCard
-            data={item}
-          />
+      data={GAMES}
+      keyExtractor={intem => intem.id}
+      renderItem={({item}) => (
+        <GameCard 
+          data={item}
+        />
         )}
         showsHorizontalScrollIndicator={false}
         horizontal
         contentContainerStyle={styles.contentList}
       />
+
+
+
+
+
     </View>
   );
 }
