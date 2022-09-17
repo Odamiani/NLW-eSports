5212981500a1e204372397f1de82e3c05fcf074b
Odamiani <feldamiani@gmail.com>
2022-09-16 23:47:33 -0300

    web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 
+import * as Dialog from '@radix-ui/react-dialog';
+
 import './styles/main.css';
 
 
@@ -7,10 +9,25 @@ import logoImg from './assets/logo-nlw-esports.svg';
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
 
+interface Game{
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
 function App() {
-  const [hasUserClickedOnButton, setHasUserClickedOnButton] = useState(false)
+  const [games, setGames] = useState<Game[]>([])
 
-
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data)
+      })
+  }, [])
 
   return(
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
@@ -20,15 +37,33 @@ function App() {
       </h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={2} />
-        <GameBanner bannerUrl="/game-2.png" title="Dota 2" adsCount={3} />
-        <GameBanner bannerUrl="/game-3.png" title="CS:GO" adsCount={4} />
-        <GameBanner bannerUrl="/game-4.png" title="Apex" adsCount={7} />
-        <GameBanner bannerUrl="/game-5.png" title="Fortinite" adsCount={5} />
-        <GameBanner bannerUrl="/game-6.png" title="World of Warcraft" adsCount={1} />
+        {games.map(game => {
+          return (
+            <GameBanner
+            key={game.id}
+            bannerUrl={game.bannerUrl} 
+            title={game.title} 
+            adsCount={game._count.ads} />
+          )
+        })}
       </div>
-      <CreateAdBanner />
+      <Dialog.Root>
+        <CreateAdBanner />
 
+        <Dialog.Portal>
+          <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
+
+          <Dialog.Content className="fixed py-8 px-10 bg-[#2A2634] text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-2xl shadow-yellow-500">
+            <Dialog.Title className="text-3xl font-black">Publique seu anúncio</Dialog.Title>
+
+            <Dialog.Content>
+              <form>
+
+              </form>
+            </Dialog.Content>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   )
 }
