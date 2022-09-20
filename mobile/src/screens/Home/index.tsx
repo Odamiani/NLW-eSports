867335c3f039ecaf266fe9231b868efaa6eb6cc0
Odamiani <feldamiani@gmail.com>
2022-09-19 22:19:03 -0300

    mobile

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from 'react';
 import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard';
 
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 
-import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  useEffect(() => {
+    fetch('http://192.168.0.7:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data))
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image 
@@ -23,7 +31,7 @@ export function Home() {
 
       
       <FlatList
-      data={GAMES}
+      data={games}
       keyExtractor={intem => intem.id}
       renderItem={({item}) => (
         <GameCard 
