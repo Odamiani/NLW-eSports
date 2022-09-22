24312ee12a2e7a39aa5bd46c6c4420adf21c2694
Odamiani <feldamiani@gmail.com>
2022-09-22 09:46:01 -0300

    Update CreateAdModal.tsx

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -24,6 +24,28 @@ export function CreateAdModal(){
         })
     }, [])
 
+    const handleCreateAd = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target as HTMLFormElement)
+
+        const data = Object.fromEntries(formData)
+
+        console.log(data)
+    }
+
+    /*
+    function handleCreateAd(event: HTMLFormElement){
+      event.preventDefault();
+
+       const formData = new FormData(event.target as unknown as HTMLFormElement)
+
+       const data = Object.fromEntries(formData)
+
+       console.log(data)
+    }
+    */
+
     return(
         <Dialog.Portal>
             <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
@@ -31,12 +53,13 @@ export function CreateAdModal(){
             <Dialog.Content className="fixed py-8 px-10 bg-[#2A2634] text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-2xl shadow-yellow-500">
                 <Dialog.Title className="text-3xl font-black">Publique seu anúncio</Dialog.Title>
 
-                <form className='mt-8 flex flex-col gap-4'>
+                <form onSubmit={handleCreateAd} className='mt-8 flex flex-col gap-4'>
             
                     <div className='flex flex-col gap-2'>
                         <label htmlFor="game" className='font-semibold'>Qual o game?</label>
                         <select 
                         id="game" 
+                        name='game'
                         className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none'
                         defaultValue=""
                         >
@@ -49,17 +72,17 @@ export function CreateAdModal(){
 
                     <div className='flex flex-col gap-2'>
                         <label htmlFor="name">Seu nome (ou nickname)</label>
-                        <Input id="name" placeholder="Como te chamam dentro do game?" />
+                        <Input name='name' id="name" placeholder="Como te chamam dentro do game?" />
                     </div>
 
                     <div className='grid grid-cols-2 gap-6' >
                         <div className='flex flex-col gap-2'>
                         <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
-                        <Input id="yearsPlaying" type="number" placeholder='Tudo bem ser ZERO' />
+                        <Input name='yearsPlaying' id="yearsPlaying" type="number" placeholder='Tudo bem ser ZERO' />
                         </div>
                         <div className='flex flex-col gap-2'>
                         <label htmlFor="discord">Qual seu discord?</label>
-                        <Input id="discord" type="text" placeholder='Usuario#0000' />
+                        <Input name='discord' id="discord" type="text" placeholder='Usuario#0000' />
                         </div>
                     </div>
 
@@ -127,8 +150,8 @@ export function CreateAdModal(){
                         <div className='flex flex-col gap-2 flex-1'>
                             <label htmlFor="hourStart">Qual o horário do dia?</label>
                             <div className='grid grid-cols-2 gap-2'>
-                                <Input id='hourStart' type="time" placeholder='De' />
-                                <Input id="hourEnd" type="time" placeholder='Até' />
+                                <Input name='hourStart' id='hourStart' type="time" placeholder='De' />
+                                <Input name='hourEnd' id="hourEnd" type="time" placeholder='Até' />
                             </div>
                         </div>
                     </div> 
